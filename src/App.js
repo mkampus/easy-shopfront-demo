d3b89fa0ed7917405b762af874f841c7d1bdcc60
Mart-Matteus Kampus <mkampus@gmail.com>
2022-04-12 16:05:58 +0300

    Cleanup and styling.

--- src/App.js
+++ src/App.js
@@ -4,21 +4,48 @@ import {
     Box,
     Button,
     Card,
-    CardActionArea,
     CardContent,
     CardMedia,
     Container,
+    createTheme,
+    FormControl,
     Grid,
     IconButton,
+    InputAdornment,
     Link,
+    OutlinedInput,
     TextField,
+    ThemeProvider,
     Typography
 } from "@mui/material";
 import {useState} from "react";
 import DeleteIcon from '@mui/icons-material/Delete';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 function App() {
+
+    //Theme
+
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: '#001219',
+            },
+            neutral: {
+                main: '#219EBC',
+            },
+            inviting: {
+                main: '#FFB703'
+            }
+        },
+    });
+
+    //Initialize UseStates
+
     const [textValue1, setTextValue1] = useState("");
     const [textValue2, setTextValue2] = useState("");
     const [textValue3, setTextValue3] = useState("");
@@ -37,68 +64,74 @@ function App() {
                 image: 'https://static.zajo.net/content/mediagallery/zajo_dcat/image/product/types/X/9088.png'
             },
             {
-                name: 'Kickin` cloth ',
+                name: 'Kickin` Cloth ',
                 price: '50',
                 image: 'https://moodle.com/wp-content/uploads/2021/06/22087-11.jpg'
             },
             {
-                name: 'Bodacious fabric ',
+                name: 'Bodacious Fabric ',
                 price: '40',
                 image: 'https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1566927496-button-down-4-1566927485.jpg'
             },
             {
-                name: 'Cool garb ',
+                name: 'Cool Garb ',
                 price: '40',
-                image: 'https://image.uniqlo.com/UQ/ST3/WesternCommon/imagesgoods/425095/item/goods_06_425095.jpg?width=1600&impolicy=quality_75'
+                image: 'https://m.media-amazon.com/images/I/A13usaonutL._CLa%7C2140%2C2000%7C51Cn1jk9uGL.png%7C0%2C0%2C2140%2C2000%2B0.0%2C0.0%2C2140.0%2C2000.0_AC_UL1500_.png'
             },
             {
-                name: 'Rad Shirt XL',
+                name: 'Rad Shirt v1',
                 price: '10',
-                image: 'https://static.zajo.net/content/mediagallery/zajo_dcat/image/product/types/X/9088.png'
+                image: 'https://contents.mediadecathlon.com/p1484240/k$ab565f3675dbdd7e3c486175e2c16583/men-s-travel-trekking-shirt-travel100-warm-bordeaux.jpg?&f=800x800'
             },
             {
-                name: 'Kickin` cloth XL',
+                name: 'Kickin` Cloth v2',
                 price: '5',
-                image: 'https://moodle.com/wp-content/uploads/2021/06/22087-11.jpg'
+                image: 'https://imgprd19.hobbylobby.com/9/5f/26/95f264323ae49e65b2a53a909fcd7d9ee659f3c7/332Wx332H-422519-0320.jpg'
             },
             {
-                name: 'Bodacious fabric XL',
+                name: 'Bodacious Fabric v3',
                 price: '4',
-                image: 'https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1566927496-button-down-4-1566927485.jpg'
+                image: 'https://www.stoneycreek.co.nz/image/catalog/0_PRODUCT_PAGES/3_CORPORATE/1_MENS/3_MID%20LAYER/Corporate%20Shirts/Long%20Sleeve/C3224_Stoney_Creek_Corporate_Longsleeve_Shirt_Front_Black_Gingham_2000px.png'
             },
             {
-                name: 'Cool garb XL',
+                name: 'Cool Garb v4',
                 price: '9',
-                image: 'https://image.uniqlo.com/UQ/ST3/WesternCommon/imagesgoods/425095/item/goods_06_425095.jpg?width=1600&impolicy=quality_75'
+                image: 'https://4.imimg.com/data4/JT/XP/MY-27510575/mens-casual-shirts-500x500.jpg'
             }
         ]
     )
+
+    // Functions
 
     function onTextURLChange(e) {
         setTextValue1(e.target.value)
         setURL(e.target.value)
         console.log(url)
-
     }
 
-    const onTextNameChange = (e) => {
+    function onTextNameChange(e) {
         setTextValue2(e.target.value)
         setName(e.target.value);
         console.log(name)
     }
-    const onTextPriceChange = (e) => {
+
+    function onTextPriceChange(e) {
         setTextValue3(e.target.value)
         setPrice(e.target.value);
         console.log(price)
     }
 
-    const handleReset = () => {
+    // Resets inputboxes
+
+    function handleReset() {
         setTextValue1('');
         setTextValue2('')
         setTextValue3('')
     }
 
-    const uploadData = () => {
+    // Pushes/Uploads new product into grid
+
+    function uploadData() {
         let arr = [...data]
         arr.push({image: url, price: price, name: name})
         setData(arr)
@@ -106,14 +139,17 @@ function App() {
         console.log(data)
     }
 
-    const deleteProduct = (name) => {
+    // Deletes item from grid / data usestate
+
+    function deleteProduct(name) {
         // console.log(name)
         console.log(data.findIndex((data) => data.name === name));
         setData(data.filter((data) => data.name !== name));
     }
 
+    // Adds to cart
 
-    const addToCart = (price, name) => {
+    function addToCart(price, name) {
         let arr = [...shoppingCart]
         arr.push({price: price, name: name})
         setShoppingCart(arr)
@@ -121,109 +157,120 @@ function App() {
     }
 
     return (
-        <div className="App">
+        <ThemeProvider theme={theme}>
+            <div className="App">
+                <div>
+                    <Container>
 
-            <div>
+                        {/*Storefront LOGO or DESIGN would go here.*/}
 
-                <Container>
+                        <Typography
+                            variant="h1"
+                            align="center"
+                            paragraph
+                            style={{color: "#023047"}}
+                        > STOREFRONT{" "} </Typography>
+                        <Typography
+                            variant="h5"
+                            align="center"
+                            paragraph>Add your product below.</Typography>
 
-                    <Typography
-                        color="textPrimary"
-                        variant="h3"
-                        align="center"
-                        paragraph
-                    > Storefront{" "} </Typography>
+                        {/*Storefront input form starts here.*/}
 
-                    <Typography color="textPrimary"
-                                variant="h4"
-                                align="center"
-                                paragraph>Add your product below.</Typography>
+                        <Box
+                            component="form"
+                            sx={{
+                                '& > :not(style)': {m: 1, width: '25ch'},
+                            }}
+                            noValidate
+                            autoComplete="off"
+                        >
+                            <TextField id="outlined-basic1" onChange={onTextURLChange} label="Image URL"
+                                       value={textValue1}
+                                       variant="outlined"/>
+                            <TextField id="outlined-basic2" onChange={onTextNameChange} label="Product Name"
+                                       value={textValue2} variant="outlined"/>
 
+                            {/*A different way to do it just for fun.*/}
 
-                    <Box
-                        component="form"
-                        sx={{
-                            '& > :not(style)': {m: 1, width: '25ch'},
-                        }}
-                        noValidate
-                        autoComplete="off"
-                    >
-                        <TextField id="outlined-basic1" onChange={onTextURLChange} label="Image URL" value={textValue1}
-                                   variant="outlined"/>
-                        <TextField id="outlined-basic2" onChange={onTextNameChange} label="Product Name"
-                                   value={textValue2} variant="outlined"/>
-                        <TextField id="outlined-basic3" onChange={onTextPriceChange} label="Product Price"
-                                   value={textValue3} variant="outlined"/>
-                    </Box>
-                    <Box>
-                        <div>
-                            <Button sx={{m: 2}}
-                                    variant="contained"
-                                    component="label"
-                            >
-                                Upload Product
-                                <input onClick={uploadData}
-                                       type="button"
-                                       hidden
-                                />
-                            </Button>
-                            <IconButton aria-label="cart" sx={{float: 'right'}}>
-                                <Badge badgeContent={shoppingCart.length} color="secondary">
-                                    <ShoppingCartIcon/>
-                                </Badge>
-                            </IconButton>
-                        </div>
+                            <FormControl>
+                                <OutlinedInput id="outlined-adornment-price"
+                                               startAdornment={<InputAdornment position="start">Price</InputAdornment>}
+                                               onChange={onTextPriceChange}
+                                               value={textValue3} type="number"
+                                               inputProps={{inputMode: 'numeric', pattern: '[0-9]*'}}>
+                                </OutlinedInput>
+                            </FormControl>
 
-                    </Box>
+                        </Box>
+                        <Box>
+                            <div>
+                                <Button sx={{m: 2}}
+                                        variant="contained"
+                                        component="label"
+                                        color='neutral'
 
+                                >
+                                    Upload Product
+                                    <input onClick={uploadData}
+                                           type="button"
+                                           hidden
+                                    />
+                                </Button>
+                                <IconButton aria-label="cart" sx={{float: 'right'}}>
+                                    <Badge badgeContent={shoppingCart.length} color="secondary">
+                                        <ShoppingCartIcon/>
+                                    </Badge>
+                                </IconButton>
+                            </div>
+                        </Box>
 
-                    <Grid container spacing={1}>
+                        {/*Storefront product grid starts here.*/}
 
-                        {data.map((product, index) => (
-                            <Grid item xs={12} sm={3} key={index} label={product.name}>
-                                <Card sx={{
-                                    display: 'block',
-                                    width: '345',
-                                    boxShadow: "0 5px 8px 0 rgba(0, 0, 0, 0.3)",
-                                    backgroundColor: "#fafafa",
-                                    height: 1,
-                                    transition: "transform 0.15s ease-in-out",
-                                    "&:hover": {transform: "scale3d(1.02, 1.02, 1)"},
-                                    transformStyle: 'preserve-3d',
-                                }}>
-                                    <div align='right'>
-                                        <IconButton aria-label="delete" size="small"
-                                                    onClick={() => deleteProduct(product.name)}>
-                                            <DeleteIcon fontSize="inherit"/>
-                                        </IconButton>
-                                    </div>
-
-                                    <CardActionArea>
+                        <Grid container spacing={1}>
+                            {data.map((product, index) => (
+                                <Grid item xs={12} sm={3} key={index} label={product.name}>
+                                    <Card sx={{
+                                        display: 'block',
+                                        width: '345',
+                                        boxShadow: "0 5px 8px 0 rgba(0, 0, 0, 0.3)",
+                                        backgroundColor: "#fafafa",
+                                        height: 1,
+                                        transition: "transform 0.15s ease-in-out",
+                                        "&:hover": {transform: "scale3d(1.02, 1.02, 1)"},
+                                        transformStyle: 'preserve-3d',
+                                    }}>
+                                        <div align='right'>
+                                            <IconButton aria-label="delete" size="small"
+                                                        onClick={() => deleteProduct(product.name)}>
+                                                <DeleteIcon fontSize="inherit"/>
+                                            </IconButton>
+                                        </div>
                                         <CardMedia sx={{height: 375, maxWidth: 345,}}
                                                    image={product.image}/>
-
-                                    </CardActionArea>
-
-                                    <CardContent>
-
-                                        <Typography color="primary" variant="h5"
-                                        >
-                                            <Link href="#" underline="none"> {product.name}  </Link>
-                                        </Typography>
-                                        <Typography color="primary" variant="h5"
-                                        >
-                                            <Link href="#" underline="none"> {product.price} € </Link>
-                                        </Typography>
-                                        <Button onClick={() => addToCart(product.name, product.price)}
-                                                variant='contained'>Add to Cart</Button>
-                                    </CardContent>
-                                </Card>
-                            </Grid>
-                        ))}
-                    </Grid>
-                </Container>
+                                        <CardContent>
+                                            <Typography variant="h5"
+                                            >
+                                                <Link color='black' underline="none"> {product.name}  </Link>
+                                            </Typography>
+                                            <Typography variant="overline"
+                                            >
+                                                <Link color='black' underline="none"> {product.price} € </Link>
+                                            </Typography>
+                                            <div>
+                                                <Button color='inviting'
+                                                        onClick={() => addToCart(product.name, product.price)}
+                                                        variant='contained'>Add to Cart</Button>
+                                            </div>
+                                        </CardContent>
+                                    </Card>
+                                </Grid>
+                            ))}
+                        </Grid>
+                    </Container>
+                </div>
             </div>
-        </div>
+        </ThemeProvider>
     );
 }
 
